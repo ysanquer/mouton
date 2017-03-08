dab72491ab51ec40eb6b6003574a568b945496f8
Yann Sanquer <ysanquer@outlook.fr>
2017-03-08 13:58:28 +0100

    committing changes not staged previously

--- java-client/src/fr/upem/mouton/NamedDrawingPullSupplier.java
+++ java-client/src/fr/upem/mouton/NamedDrawingPullSupplier.java
@@ -1,27 +1,69 @@
 package fr.upem.mouton;
 
+import java.util.Objects;
+import java.util.function.Supplier;
+
 import org.omg.CORBA.Any;
 import org.omg.CORBA.BooleanHolder;
+import org.omg.CORBA.ORB;
 import org.omg.CosEventComm.Disconnected;
 import org.omg.CosEventComm.PullSupplierOperations;
 
-public class NamedDrawingPullSupplier extends Thread implements PullSupplierOperations {
+public class NamedDrawingPullSupplier implements PullSupplierOperations {
+	private final Supplier<Any> createAny;
+	private NamedDrawing toSend = null;
+	private boolean sending = false;
+	private final Object sync = new Object();
+
+	public boolean isSending() {
+		return sending;
+	}
+
+	public void sendNamedDrawing(NamedDrawing nd) {
+		synchronized (sync) {
+			if (isSending()) {
+				throw new IllegalStateException("already sending");
+			}
+			this.toSend = Objects.requireNonNull(nd);
+			this.sending = true;
+		}
+	}
+
+	public NamedDrawingPullSupplier(Supplier<Any> createAny) {
+		this.createAny = Objects.requireNonNull(createAny);
+	}
 
 	@Override
 	public void disconnect_pull_supplier() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("NAMED_DRAWING_PULL_SUPPLIER/disconnect");
 	}
 
 	@Override
 	public Any pull() throws Disconnected {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("NAMED_DRAWING_PULL_SUPPLIER/pull begin");
+		BooleanHolder has_event = new BooleanHolder(false);
+		Any ret;
+		do {
+			ret = this.try_pull(has_event);
+		} while (!has_event.value);
+		System.out.println("NAMED_DRAWING_PULL_SUPPLIER/pull end");
+		return ret;
 	}
 
 	@Override
-	public Any try_pull(BooleanHolder arg0) throws Disconnected {
-		// TODO Auto-generated method stub
-		return null;
+	public Any try_pull(BooleanHolder has_event) throws Disconnected {
+		System.out.println("NAMED_DRAWING_PULL_SUPPLIER/try_pull begin");
+		Any ret = this.createAny.get();
+		has_event.value = false;
+		synchronized (sync) {
+			if (sending) {
+				NamedDrawingHelper.insert(ret, toSend);
+				has_event.value = true;
+				sending = false;
+				toSend = null;
+			}
+		}
+		System.out.println("NAMED_DRAWING_PULL_SUPPLIER/try_pull end");
+		return ret;
 	}
 }
--- java-client/src/fr/upem/mouton/main/MoutonClientMain.java
+++ java-client/src/fr/upem/mouton/main/MoutonClientMain.java
@@ -1,7 +1,60 @@
 package fr.upem.mouton.main;
 
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosEventChannelAdmin.AlreadyConnected;
+import org.omg.CosEventChannelAdmin.ConsumerAdmin;
+import org.omg.CosEventChannelAdmin.EventChannel;
+import org.omg.CosEventChannelAdmin.EventChannelHelper;
+import org.omg.CosEventChannelAdmin.ProxyPullConsumer;
+import org.omg.CosEventChannelAdmin.ProxyPushSupplier;
+import org.omg.CosEventChannelAdmin.SupplierAdmin;
+import org.omg.CosEventChannelAdmin.TypeError;
+import org.omg.CosEventComm.PullSupplier;
+import org.omg.CosEventComm.PullSupplierHelper;
+import org.omg.CosEventComm.PullSupplierPOATie;
+import org.omg.CosEventComm.PushConsumer;
+import org.omg.CosEventComm.PushConsumerHelper;
+import org.omg.CosEventComm.PushConsumerPOATie;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAPackage.ServantNotActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
+
+import fr.upem.mouton.AnnotatedDrawingPushConsumer;
+import fr.upem.mouton.NamedDrawingPullSupplier;
+
 public class MoutonClientMain {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	public static void main(String[] args)
+			throws InvalidName, NotFound, CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName,
+			AdapterInactive, ServantNotActive, WrongPolicy, AlreadyConnected, TypeError {
+		ORB orb = ORB.init(args, null);
+		NamingContextExt nc = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+
+		EventChannel ecs = EventChannelHelper.narrow(nc.resolve(nc.to_name("EventChannel")));
+
+		ConsumerAdmin ca = ecs.for_consumers();
+		SupplierAdmin sa = ecs.for_suppliers();
+
+		ProxyPushSupplier pps = ca.obtain_push_supplier();
+		ProxyPullConsumer ppc = sa.obtain_pull_consumer();
+
+		POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		rootPOA.the_POAManager().activate();
+
+		PushConsumerPOATie pcTie = new PushConsumerPOATie(new AnnotatedDrawingPushConsumer());
+		PullSupplierPOATie psTie = new PullSupplierPOATie(new NamedDrawingPullSupplier(orb::create_any));
+
+		PushConsumer pc = PushConsumerHelper.narrow(rootPOA.servant_to_reference(pcTie));
+		PullSupplier ps = PullSupplierHelper.narrow(rootPOA.servant_to_reference(psTie));
+		pps.connect_push_consumer(pc);
+		ppc.connect_pull_supplier(ps);
+		System.out.println("servants registered");
+		orb.run();
 	}
 };
